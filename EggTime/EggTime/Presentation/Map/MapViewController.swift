4986c79aa1fe88ce2d8eb0054b998dd12ffb7166
useok <useok056900@naver.com>
2022-09-21 21:44:40 +0900

    feat: 가까운 맵들 hidden 처리

--- EggTime/EggTime/Presentation/Map/MapViewController.swift
+++ EggTime/EggTime/Presentation/Map/MapViewController.swift
@@ -10,87 +10,92 @@ import NMapsMap
 import CoreLocation
 import SnapKit
 import RealmSwift
+import Cluster
 
 class MapViewController: BaseViewController,NMFMapViewCameraDelegate, CLLocationManagerDelegate, NMFMapViewOptionDelegate {
-    
+
     let mapview = MapView()
-    
+
     override func loadView() {
         super.view = mapview
     }
-    
+
     var locationManager = CLLocationManager()
     var latitude: Double?
     var longtitude: Double?
-    
+
     let repository = RealmRepository()
     var tasks: Results<EggTime>!
-    
+
     override func viewWillAppear(_ animated: Bool) {
         print(#function)
         tasks = repository.fetch()
         setpin()
+        
         if CLLocationManager.locationServicesEnabled() {
             print("위치 서비스 On 상태")
             locationManager.startUpdatingLocation()
-            
+
             //기본위치
             let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: 37.553409 , lng: 126.969734 )) // 서울역위치
             cameraUpdate.animation = .easeIn
             naverMapView.mapView.moveCamera(cameraUpdate)
-            
+
         } else {
             print("위치 서비스 Off 상태")
         }
     }
-    
+
     var distanceArray: [CLLocationDistance] = []
     var markers = [NMFMarker]()
     let infoWindow = NMFInfoWindow()
     let dataSource = NMFInfoWindowDefaultTextSource.data()
     let sdkBundle = Bundle.naverMapFramework()
+    
+    
+    let clusterManager = ClusterManager()
 
 
     lazy var naverMapView = NMFNaverMapView(frame: view.frame) // UIView
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "mappin.and.ellipse"), style: .plain, target: self, action:#selector(tapsoakButton) )
-        
+
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
 
-        
+
         naverMapView.mapView.zoomLevel = 17
         naverMapView.mapView.positionMode = .direction
-        
+
 //        naverMapView.mapView.touchDelegate = self
         naverMapView.mapView.addCameraDelegate(delegate: self)
         naverMapView.mapView.addOptionDelegate(delegate: self)
         naverMapView.mapView.isIndoorMapEnabled = true //실내지도
         
         view.addSubview(naverMapView)
-        
+
         naverMapView.snp.makeConstraints {
             $0.edges.equalTo(view.safeAreaLayoutGuide)
         }
-        
+
     }
-    
+
 
 }
 
 extension MapViewController {
-    
 
-    
+
+
     @objc func tapsoakButton(){
         let vc = WriteViewController()
         transition(vc,transitionStyle: .push)
     }
-    
+
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         print(#function)
         if let location = locations.first {
@@ -98,10 +103,10 @@ extension MapViewController {
             print("현재좌표:",location.coordinate.latitude)
             print("현재좌표:",location.coordinate.longitude)
             distanceArray.removeAll()
-            
+
             naverMapView.showLocationButton = true
             naverMapView.mapView.positionMode = .direction
-            
+
             tasks.forEach{
                 //MARK: 거리 계산하는 매소드
                 let containDistance = location.distance(from: CLLocation(latitude: CLLocationDegrees($0.latitude ?? 0), longitude: CLLocationDegrees($0.longitude ?? 0)))
@@ -120,26 +125,26 @@ extension MapViewController {
 
             UserDefaults.standard.set(location.coordinate.latitude, forKey: "lat")
             UserDefaults.standard.set(location.coordinate.longitude, forKey: "lng")
-            
-            
-            
-            
+
+
+
+
         }
 
         // 위치 업데이트 멈춰 (실시간성이 중요한거는 매번쓰고, 중요하지않은건 원하는 시점에 써라)
         locationManager.stopUpdatingLocation() // stopUpdatingHeading 이랑 주의
 
     }
-    
-    
-    
+
+
+
     // 탭할떄
 
     func setpin() {
         if !tasks.isEmpty {
             tasks.forEach { task in
                 let marker = NMFMarker(position: NMGLatLng(lat: task.latitude!, lng: task.longitude!))
-                
+                marker.isHideCollidedMarkers = true // 여러개 모여있으면 숨겨주는기능
                 marker.captionText = task.title
                 marker.touchHandler = { (overlay) in
                     if let marker = overlay as? NMFMarker {
@@ -149,9 +154,9 @@ extension MapViewController {
                                 self.view.addSubview($0)
                             }
                             self.show()
-                            
+
                             self.mapview.image.contentMode = .scaleToFill
-                            
+
                             if task.imageList.count != 0 {
                                 self.mapview.image.image = self.loadImageFromDocument(fileName: task.imageList[0])
                             } else {
@@ -165,22 +170,22 @@ extension MapViewController {
                          }
                     }
                     return true
-                    
+
                 }
-            
+
                 marker.mapView = naverMapView.mapView
                 infoWindow.open(with: marker)
-                
-                
+
+
             }
         }
     }
-    
+
     @objc func checkButtonClicked() {
         hidden()
     }
     func show() {
-        
+
         self.mapview.popup.snp.remakeConstraints {
             $0.edges.equalTo(self.view)
         }
@@ -197,7 +202,7 @@ extension MapViewController {
         self.mapview.checkButton.addTarget(self, action: #selector(self.checkButtonClicked), for: .touchUpInside)
     }
 
-    
+
     func hidden() {
         self.mapview.popup.snp.remakeConstraints {
             $0.height.width.equalTo(0)
