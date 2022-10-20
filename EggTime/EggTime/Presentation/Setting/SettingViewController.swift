1391a4fccb38cbdf3bf108451648b31cc017157b
Wooseok <wooseokbird.dev@gmail.com>
2022-10-20 09:44:18 +0900

    fix: setting -> differble CollectionView

--- EggTime/EggTime/Presentation/Setting/SettingViewController.swift
+++ EggTime/EggTime/Presentation/Setting/SettingViewController.swift
@@ -3,14 +3,17 @@
 import UIKit
 
 class SettingViewController: BaseViewController {
-
+    
     //MARK: 뷰 가져오기
     let settingview = SettingView()
+    
+    private var dataSource: UICollectionViewDiffableDataSource<Int,String>!
     
     override func loadView() {
         super.view = settingview
     }
-
+    
+    
     override func viewWillAppear(_ animated: Bool) {
         self.navigationItem.title = "설정"
     }
@@ -21,27 +24,77 @@ class SettingViewController: BaseViewController {
         
         
         self.navigationItem.backBarButtonItem = backBarButtonItem
-        settingview.tableView.dataSource = self
-        settingview.tableView.delegate = self
-
-
-
+        
+        settingview.collectionview.collectionViewLayout = createLayout()
+        
+        configureDataSource()
+        settingview.collectionview.delegate = self
     }
-    @objc func listClicked() {
-        let vc = ListViewController()
-        transition(vc,transitionStyle: .presentFullNavigation)
-    }
-    @objc func homeClicked() {
-        let vc = MainViewController()
-        transition(vc,transitionStyle: .presentFullNavigation)
-    }
-    // 지도보기 버튼
-    @objc func mapShowButtonClicked() {
-        let vc = MapViewController()
-        transition(vc,transitionStyle: .push)
-    }
-
-
 }
 
 
+
+extension SettingViewController {
+    private func createLayout() -> UICollectionViewLayout {
+        var config = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        config.backgroundColor = .clear
+        let layout = UICollectionViewCompositionalLayout.list(using: config)
+        return layout
+    }
+    
+    private func configureDataSource() {
+        var cellRegisteration = UICollectionView.CellRegistration<UICollectionViewCell,String> (handler: {cell, indexPath, itemIdentifier in
+            var content = UIListContentConfiguration.valueCell()
+            content.text = Setting.allCases[indexPath.section].list[indexPath.row]
+            content.textProperties.color = .white
+            cell.contentConfiguration = content
+            cell.backgroundConfiguration?.backgroundColor = .green
+            
+            var background = UIBackgroundConfiguration.listPlainCell()
+            background.backgroundColor = .clear
+            
+            cell.backgroundConfiguration = background
+            
+        })
+        
+        // cv.
+        dataSource = UICollectionViewDiffableDataSource(collectionView: settingview.collectionview, cellProvider: { collectionView, indexPath, itemIdentifier in
+            let cell = collectionView.dequeueConfiguredReusableCell(using: cellRegisteration, for: indexPath, item: itemIdentifier) // indexPath, itemIdentifier 둘다 이용해서 cell에 접근가능
+            return cell
+        })
+        
+        var snapShot = NSDiffableDataSourceSnapshot<Int,String>()
+        snapShot.appendSections([0])
+        snapShot.appendItems(Setting.content.list)
+        dataSource.apply(snapShot)
+        
+        
+    }
+}
+
+extension SettingViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        switch indexPath.item {
+        case 0 :
+            let vc = BackupStoredViewController()
+            transition(vc,transitionStyle: .push)
+        case 1:
+            let vc = OpenLicenseViewController()
+            transition(vc,transitionStyle: .push)
+        case 2:
+            if let appstoreUrl = URL(string: "https://apps.apple.com/app/id\(1645004650)") {
+                var urlComp = URLComponents(url: appstoreUrl, resolvingAgainstBaseURL: false)
+                urlComp?.queryItems = [
+                    URLQueryItem(name: "action", value: "write-review")
+                ]
+                guard let reviewUrl = urlComp?.url else {
+                    return
+                }
+                UIApplication.shared.open(reviewUrl, options: [:], completionHandler: nil)
+            }
+            
+        default:
+            break
+        }
+    }
+}
--- EggTime/EggTime/Presentation/Setting/View/SettingView.swift
+++ EggTime/EggTime/Presentation/Setting/View/SettingView.swift
@@ -19,26 +19,34 @@ class SettingView: BaseView {
         fatalError("init(coder:) has not been implemented")
     }
     //MARK: 크기
-    let tableView : UITableView = {
-        let tableview = UITableView(frame: .zero, style: .insetGrouped)
-        tableview.register(SettingTableViewCell.self, forCellReuseIdentifier: SettingTableViewCell.reuseIdentifier )
-        tableview.rowHeight = 80
-        tableview.backgroundColor = .clear
-        return tableview
+    let collectionview : UICollectionView = {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
         
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .absolute(44))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                       subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        let spacing : CGFloat = 8
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        return cv
     }()
+    
+    
     //MARK: 뷰등록
     
     override func configure() {
-        [tableView].forEach {
+        [collectionview].forEach {
             self.addSubview($0)
         }
     }
     
-    
     //MARK: 위치
     override func setConstrains() {
-        tableView.snp.makeConstraints {
+        collectionview.snp.makeConstraints {
             $0.top.equalTo(self.safeAreaLayoutGuide)
             $0.leading.trailing.equalTo(self.safeAreaLayoutGuide)
             $0.bottom.equalTo(self.safeAreaLayoutGuide)
@@ -48,49 +56,49 @@ class SettingView: BaseView {
 }
 
 
-extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return Setting.allCases.count
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Setting.allCases[section].list.count
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingTableViewCell.reuseIdentifier, for: indexPath) as? SettingTableViewCell else {
-            return UITableViewCell()
-        }
-        cell.content.text = Setting.allCases[indexPath.section].list[indexPath.row]
-        cell.backgroundColor = .clear
-        cell.selectionStyle = .none // 버튼클릭시 색상변화 x
-        return cell
-    }
-    
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        switch indexPath.row {
-        case 0: // 백업하기
-            let vc = BackupStoredViewController()
-            transition(vc,transitionStyle: .push)
-        case 1: //오픈라인센스
-            let vc = OpenLicenseViewController()
-            transition(vc,transitionStyle: .push)
-        case 2: //리뷰쓰기
-            if let appstoreUrl = URL(string: "https://apps.apple.com/app/id\(1645004650)") {
-                var urlComp = URLComponents(url: appstoreUrl, resolvingAgainstBaseURL: false)
-                urlComp?.queryItems = [
-                    URLQueryItem(name: "action", value: "write-review")
-                ]
-                guard let reviewUrl = urlComp?.url else {
-                    return
-                }
-                UIApplication.shared.open(reviewUrl, options: [:], completionHandler: nil)
-            }
-        default:
-            break
-        }
-    }
-    
-    
-}
+//extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
+//
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return Setting.allCases.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return Setting.allCases[section].list.count
+//    }
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingTableViewCell.reuseIdentifier, for: indexPath) as? SettingTableViewCell else {
+//            return UITableViewCell()
+//        }
+//        cell.content.text = Setting.allCases[indexPath.section].list[indexPath.row]
+//        cell.backgroundColor = .clear
+//        cell.selectionStyle = .none // 버튼클릭시 색상변화 x
+//        return cell
+//    }
+//
+//
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        switch indexPath.row {
+//        case 0: // 백업하기
+//            let vc = BackupStoredViewController()
+//            transition(vc,transitionStyle: .push)
+//        case 1: //오픈라인센스
+//            let vc = OpenLicenseViewController()
+//            transition(vc,transitionStyle: .push)
+//        case 2: //리뷰쓰기
+//            if let appstoreUrl = URL(string: "https://apps.apple.com/app/id\(1645004650)") {
+//                var urlComp = URLComponents(url: appstoreUrl, resolvingAgainstBaseURL: false)
+//                urlComp?.queryItems = [
+//                    URLQueryItem(name: "action", value: "write-review")
+//                ]
+//                guard let reviewUrl = urlComp?.url else {
+//                    return
+//                }
+//                UIApplication.shared.open(reviewUrl, options: [:], completionHandler: nil)
+//            }
+//        default:
+//            break
+//        }
+//    }
+//
+//
+//}
